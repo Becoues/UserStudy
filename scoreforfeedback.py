e0277eb6a67509957f0a04db10ba7b01f925cb08
Zifeng Liu <66907845+Becoues@users.noreply.github.com>
2024-11-24 10:42:37 +0800

    Add files via upload

--- scoreforfeedback.py
+++ scoreforfeedback.py
@@ -25,7 +25,7 @@ def score_for_feedback(feedbacks,a,b,c):
     total_score = max(total_score, 0)
 
     # 将百分制分数标准化到 -5 到 +5 的区间
-    normalized_score = (total_score - 50) * 0.1
+    normalized_score = (total_score - 50) * 0.1-5
     return normalized_score
 
 # feedbacks_low = [
@@ -35,17 +35,17 @@ def score_for_feedback(feedbacks,a,b,c):
 #     "A模型的行程太长了",                            # 未提到具体的耗时店铺，缺少具体信息
 #     "B模型推荐的店铺太重复了"                        # 未列举具体重复的店铺名
 # ]
-feedbacks_high = [
-    "模型A推荐了'ZARA', 'Nike Kicks Lounge' 和 'GUESS'，非常符合我的购物兴趣，既有服装也有运动鞋", # 具体提及符合兴趣的店铺名
-    "模型B的路径从'SEPHORA'到'星巴克'再到'Nike Kicks Lounge', 行程连贯而且没有多余的走动，非常便利", # 明确指出无绕路的行程段，具体店铺提及
-    "考虑到时间限制，模型A推荐的店铺'小米之家'与'Miss Sixty'距离较近，可以节省大量时间", # 提及具体的时间节省店铺
-    "模型B推荐的行程包含了服装店'GUESS', 鞋店'FILA'以及饮品店'喜茶'，确保了行程的多样性", # 指出行程多样性并列出具体店铺名
-    "模型A多次推荐了服装店，包括'ZARA', 'Tommy Hilfiger'和'Armani Exchange'，这超出我的需求并让我感到有些乏味", # 指出重复推荐和具体店铺名
-]
-a = "均无"
-b = "模型A"
-c = "均无"
-# final_score = score_for_feedback(feedbacks_low)
+# feedbacks_high = [
+#     "模型A推荐了'ZARA', 'Nike Kicks Lounge' 和 'GUESS'，非常符合我的购物兴趣，既有服装也有运动鞋", # 具体提及符合兴趣的店铺名
+#     "模型B的路径从'SEPHORA'到'星巴克'再到'Nike Kicks Lounge', 行程连贯而且没有多余的走动，非常便利", # 明确指出无绕路的行程段，具体店铺提及
+#     "考虑到时间限制，模型A推荐的店铺'小米之家'与'Miss Sixty'距离较近，可以节省大量时间", # 提及具体的时间节省店铺
+#     "模型B推荐的行程包含了服装店'GUESS', 鞋店'FILA'以及饮品店'喜茶'，确保了行程的多样性", # 指出行程多样性并列出具体店铺名
+#     "模型A多次推荐了服装店，包括'ZARA', 'Tommy Hilfiger'和'Armani Exchange'，这超出我的需求并让我感到有些乏味", # 指出重复推荐和具体店铺名
+# ]
+# # a = "均无"
+# # b = "模型A"
+# # c = "均无"
+# # final_score = score_for_feedback(feedbacks_low)
+# # print("Your feedback score adjusted reward: ", final_score)
+# final_score = score_for_feedback(feedbacks_high,a,b,c)
 # print("Your feedback score adjusted reward: ", final_score)
-final_score = score_for_feedback(feedbacks_high,a,b,c)
-print("Your feedback score adjusted reward: ", final_score)
